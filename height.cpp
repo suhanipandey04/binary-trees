db276b156e827ed96591c58d1a603e9c6fb3ad14
suhanipandey04 <dheeverse@gmail.com>
2025-12-01 20:19:14 +0530

    6

---
+++ height.cpp
@@ -0,0 +1,55 @@
+//height of binarey tree which can also be called as maximum depth of binary tree
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node
+{
+    public:
+      int data;
+      Node* left;
+      Node* right; 
+
+      Node(int val)
+      {
+        data=val;
+        left=right=NULL;
+      }
+
+};
+static int idx=-1;
+Node* buildTree(vector<int>& preorder)
+{
+     idx++;
+     if(preorder[idx]==-1)
+     {
+        return NULL;
+     }
+     Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder);//left
+    root->right = buildTree(preorder);//right
+
+    return root;
+
+}
+
+int maxDepth(Node* root) {
+        if(root==NULL)
+           return 0;
+        int leftdepth=maxDepth(root->left);
+        int rightdepth=maxDepth(root->right);
+
+        return max(leftdepth,rightdepth)+1;
+
+        
+    }
+
+int main()
+{
+   vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+   Node* root = buildTree(preorder);
+
+   cout<<"MaxDepth : "<<maxDepth(root) << endl;
+   
+   return 0;
+}  
